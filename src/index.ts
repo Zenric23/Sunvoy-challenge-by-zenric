5aa3d9616b1d4328c962405cb7a30ae1b0bf0d27
zenric-atlasopen <zenric@atlasopen.com>
2025-06-15 19:16:20 +0800

    Fetch currently authenticated user

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { fetchUser } from './fetchUser';
+import { fetchCurrentAuthenticatedUser } from './fetchCurrentAuthenticatedUser';
 
 const app = express();
 
 fetchUser();
+fetchCurrentAuthenticatedUser();
 
 app.listen(8000, () => {
   console.log(`Server running on port 8000`);
